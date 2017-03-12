66c049781190eec76de1dae8df1d5d89639f21b4
LuoMing Xu <xjy46566696@163.com>
2017-03-12 15:15:36 +0800

    补充二维数组代码及错误返回代码的实验

--- ForLingSijia/Code.c
+++ ForLingSijia/Code.c
@@ -1,7 +1,11 @@
 ﻿#include<stdio.h>
 #include<malloc.h>
 #include<time.h>
+#include<string.h>
+#include<errno.h>
 
+int** askForArray2D(int rows, int columns);//定义二维数组
+void freeMalloc2D(int** array, int rows);//记得要free，malloc
 long long another(void);
 
 //将输入的数组按依次移位打印
@@ -12,18 +16,56 @@ int main(int argc, char const *argv[])
 	double duration;
 	
 	int *array;
-	int displacement = 0;
-    int length = 0;
-    int num = 0;
-    int choose = 0;
+	int displacement = 0;//位移
+	int length = 0;//一维数组长度
+	int num = 0;
+	int choose = 0;
 	int i = 0;
 
 	printf("输入数组的长度，和位移的大小，以\",\"隔开\n");
 	scanf_s("%d,%d", &length, &displacement);
-	//定义数组长度
-	array = (int*)malloc(sizeof(int)*length);
+	//定义一维数组长度
+	array = (int*)malloc(sizeof(int*)*length);
 
-	for (i; i<length; i++)
+	//定义二维数组
+	{
+		int rows = 0;
+		int columns = 0;
+		int** array;
+
+		printf("请输入行与列以\"，\"隔开\n");
+		scanf_s("%d,%d", &rows, &columns);
+
+		array=askForArray2D(rows, columns);
+		
+		for (int i = 0; i < rows; i++)
+		{
+			for (int j = 0; j < columns; j++)
+			{
+				array[i][j] = i*columns + j;
+				printf("array[%d][%d]=%d\t", i, j, array[i][j]);
+			}
+			printf("\n");
+		}
+		
+		for (int i = 0; i < rows; i++)
+			for (int j = 0; j < columns; j++)
+				array[i][j] = i*columns + j;
+
+		for (int i = 0; i < rows; i++)
+		{
+			for (int j = 0; j < columns; j++)
+				printf("array[%d][%d]=%2d\t", i, j, array[i][j]);
+			printf("\n");
+		}
+			
+
+
+
+		freeMalloc2D(array, rows);
+	}
+
+	for (i; i < length; i++)
 	{
 		printf("开始输入数组第%d个:", i + 1);
 		scanf_s("%d", &num);
@@ -40,7 +82,7 @@ int main(int argc, char const *argv[])
 			printf("%d ", array[i + displacement - length]);
 	}
 
-	{	
+	{
 		//打印运行的时间
 		finish = clock();//计时结束
 		duration = (double)(finish - start) / CLOCKS_PER_SEC;
@@ -50,7 +92,7 @@ int main(int argc, char const *argv[])
 	printf("\n如果你想做another，请输入1，不想请输入任一非1的数\n");
 	scanf_s("%d", &choose);
 	start = clock();//计时开始
-	if(choose==1)
+	if (choose == 1)
 		printf("%lld\n", another());
 	{
 		//打印运行的时间
@@ -77,4 +119,29 @@ long long another(void)
 	}
 
 	return sum;
+}
+int** askForArray2D(int rows, int columns)//P.S.里面有乘号...
+{
+	int **array;
+	char* strerroe(int errnum);
+	array = (int**)malloc(sizeof(int*)*rows);//先获取行指针
+	if (NULL == array)
+		perror("error:");
+	exit(1);
+	for (int i = 0; i < rows; i++)
+	{
+		array[i] = (int*)malloc(sizeof(int)*columns);//再定义列指针
+		if (NULL == array)
+			perror("error:");
+	}
+
+
+	return array;
+}
+void freeMalloc2D(int** array, int rows)
+{
+	for (int i = 0; i < rows; i++)
+		free(array[i]);
+
+	free(array);
 }
